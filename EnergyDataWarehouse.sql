454295cafa4c18fea7977792e159eb5a865ef6b1
João Rocha <joaonolascorocha@tecnico.ulisboa.pt>
2024-10-24 18:26:40 +0100

    Updated: EnergyDataWarehouse

--- EnergyDataWarehouse.sql
+++ EnergyDataWarehouse.sql
@@ -18,10 +18,11 @@ CREATE TABLE dim_time (
 
 CREATE TABLE dim_location (
     location_id INT PRIMARY KEY AUTO_INCREMENT,
-    region VARCHAR(255) NOT NULL,
+    district VARCHAR(255) NOT NULL,
     municipality VARCHAR(255) NOT NULL,
     parish VARCHAR(255),
-    UNIQUE KEY (region, municipality, parish)
+    municipalityparishcode INT,
+    UNIQUE KEY (district, municipality, parish, municipalityparishcode)
 );
 
 -- Fact tables
@@ -30,8 +31,10 @@ CREATE TABLE fact_energy_consumption (
     fact_id INT PRIMARY KEY AUTO_INCREMENT,
     time_id INT NOT NULL,
     location_id INT NOT NULL,
+    municipalityparishcode INT NOT NULL,
     energy_consumption DECIMAL(15, 2) NOT NULL,
-    percentage_smart_meters DECIMAL(5, 2) NOT NULL,
+    count_smart INT NOT NULL,
+    count_non_smart INT NOT NULL,
     FOREIGN KEY (time_id) REFERENCES dim_time(time_id),
     FOREIGN KEY (location_id) REFERENCES dim_location(location_id)
 );
